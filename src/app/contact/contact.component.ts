c8af186b659e6b65dc861914056bbb4985efe5f6
Julian Gonzalez Prieto <iguanodontecazador@gmail.com>
2024-02-07 23:48:40 -0800

    Contact page - form validations

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,31 +1,47 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MatButtonModule],
+  imports: [FormsModule, ReactiveFormsModule, MatButtonModule, NgIf],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
   contactForm = this.formBuilder.group({
-    name: '',
-    email: '',
-    website: '',
-    phone: '',
-    message: ''
+    name: new FormControl('', [Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+    website: new FormControl('', [Validators.pattern("(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?")]),
+    phone: new FormControl('', [Validators.minLength(7), Validators.pattern("^[0-9]*$")]),
+    message: new FormControl('', [Validators.required, Validators.minLength(5)]),
   });
 
   constructor(
     private formBuilder: FormBuilder,
   ) { }
 
+  ngOnInit(): void {
+  }
+
   onSubmit(): void {
-    console.warn('Your mail has been sent', this.contactForm.value);
-    this.contactForm.reset();
+    if (this.contactForm.valid) {
+      // Here is the logic for send email
+
+      console.warn('Your mail has been sent', this.contactForm.value);
+      this.contactForm.reset();
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
+
+  onlyNumbers(event: any) {
+    if (event.key !== null && ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].indexOf(event.key) === -1) {
+      event.preventDefault();
+    }
   }
 
 }
