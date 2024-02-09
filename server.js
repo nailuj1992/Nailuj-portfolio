a566f86ffb51445114e7c52f573add48d4745440
Julian Gonzalez Prieto <iguanodontecazador@gmail.com>
2024-02-09 14:02:10 -0800

    /dist/nailuj-portfolio/browser

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/nailuj-portfolio'));
+app.use(express.static(__dirname + '/dist/nailuj-portfolio/browser'));
 
 app.get('/*', function (req, res) {
 
