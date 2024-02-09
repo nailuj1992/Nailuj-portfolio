4c7b46b373b8478e40777714d2dc8cdce978e769
Julian Gonzalez Prieto <iguanodontecazador@gmail.com>
2024-02-09 01:45:56 -0800

    back

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/nailuj-portfolio'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/nailuj-portfolio/browser/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/nailuj-portfolio/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
