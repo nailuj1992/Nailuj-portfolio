b7d819f149627f78a259aa060485ecce545ca683
Julian Gonzalez Prieto <iguanodontecazador@gmail.com>
2024-02-09 12:37:53 -0800

    server.js

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // app.use(express.static(__dirname + '/dist/nailuj-portfolio'));
-app.use(express.static(path.join(__dirname, 'dist','app_name')));
+app.use(express.static(path.join(__dirname, 'dist','nailuj-portfolio')));
 
 app.get('/*', function (req, res) {
 
     // res.sendFile(path.join(__dirname + '/dist/nailuj-portfolio/browser/index.html'));
-    res.sendFile(path.join(__dirname,'dist','app_name','index.html'));
+    res.sendFile(path.join(__dirname,'dist','nailuj-portfolio','index.html'));
 });
 
 // Start the app by listening on the default Heroku port
