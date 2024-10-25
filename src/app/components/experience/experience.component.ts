08606b98e223f7dbe59aaac1606eaabe43a5e93d
Julian Gonzalez Prieto <juliangonzalezprieto@hotmail.com>
2024-10-25 18:26:44 -0500

    Condensed the experience section into a slides component

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,12 +1,15 @@
 import { Component, Inject } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { DOCUMENT, CommonModule } from '@angular/common';
 import { getText } from '@model/dto/language';
 import { environment } from '@environments/environment';
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { MatButtonModule } from '@angular/material/button';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule, MatButtonModule],
   templateUrl: 'experience.component.html',
   styleUrl: 'experience.component.css'
 })
@@ -14,7 +17,99 @@ export class ExperienceComponent {
 
   env = environment;
 
-  constructor(@Inject(DOCUMENT) readonly document: Document) { }
+  slides: any[];
+  animationSpeed = 300;
+
+  currentSlide = 0;
+  faArrowRight = faArrowRight;
+  faArrowLeft = faArrowLeft;
+  hidden = false;
+
+  constructor(@Inject(DOCUMENT) readonly document: Document) {
+    this.slides = [
+      {
+        title: "experience-1-job",
+        duration: "experience-1-duration",
+        link: this.env.helpUsDefendLink,
+        nameLink: "Help us Defend",
+        place: "Vancouver, BC",
+        isRemote: true,
+        technologies: ["csharp", "unity", "git"]
+      },
+      {
+        title: "experience-2-job",
+        duration: "experience-2-duration",
+        link: this.env.globalHitssLink,
+        nameLink: "Global Hitss S.A",
+        place: "Bogotá D.C, Colombia",
+        isRemote: false,
+        technologies: ["java", "javascript", "html", "css", "git", "maven", "oracle"]
+      },
+      {
+        title: "experience-3-job",
+        duration: "experience-3-duration",
+        link: this.env.bancoDeBogotaLink,
+        nameLink: "Banco de Bogotá",
+        place: "Bogotá D.C, Colombia",
+        isRemote: false,
+        technologies: ["java", "typescript", "html", "css", "springboot", "nodejs", "angular", "git", "maven", "mysql", "redis"]
+      },
+      {
+        title: "experience-4-job",
+        duration: "experience-4-duration",
+        link: this.env.sistemasComercialesLink,
+        nameLink: "Sistemas Comerciales SIA Ltda",
+        place: "Bogotá D.C, Colombia",
+        isRemote: false,
+        technologies: ["java", "php", "javascript", "html", "css", "yii", "git", "maven", "xampp", "oracle", "mysql"]
+      },
+      {
+        title: "experience-5-job",
+        duration: "experience-5-duration",
+        link: this.env.dataGlobalLink,
+        nameLink: "Data Global S.A.S",
+        place: "Bogotá D.C, Colombia",
+        isRemote: false,
+        technologies: ["php", "javascript", "html", "css", "yii", "git", "xampp", "mysql"]
+      },
+      {
+        title: "experience-6-job",
+        duration: "experience-6-duration",
+        link: this.env.sistemasComercialesLink,
+        nameLink: "Sistemas Comerciales SIA Ltda",
+        place: "Bogotá D.C, Colombia",
+        isRemote: false,
+        technologies: ["java", "git", "maven", "oracle"]
+      },
+      {
+        title: "experience-7-job",
+        duration: "experience-7-duration",
+        link: this.env.sistemasComercialesLink,
+        nameLink: "Sistemas Comerciales SIA Ltda",
+        place: "Bogotá D.C, Colombia",
+        isRemote: false,
+        technologies: ["php", "javascript", "html", "css", "xampp", "mysql"]
+      }
+    ];
+  }
+
+  next() {
+    let currentSlide = (this.currentSlide + 1) % this.slides.length;
+    this.jumpToSlide(currentSlide);
+  }
+
+  previous() {
+    let currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+    this.jumpToSlide(currentSlide);
+  }
+
+  jumpToSlide(index: number) {
+    this.hidden = true;
+    setTimeout(() => {
+      this.currentSlide = index;
+      this.hidden = false;
+    }, this.animationSpeed);
+  }
 
   getText(id: string) {
     const sessionStorage = this.document.defaultView?.sessionStorage;
