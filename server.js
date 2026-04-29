bc42603e148be6b45510c2cf6bc17e22bfa66d4e
pegasus1992 <iguanodontecazador@gmail.com>
2026-04-28 21:53:48 -0500

    fixed some vulnerabilities

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const path = require('path');
 
 const app = express();
 
+// Sanitize request URL to prevent SSRF via protocol-relative and backslash URLs
+// See: https://github.com/angular/angular/security/advisories/GHSA-x7j2-3qr7-5x3h
+app.use((req, res, next) => {
+    if (req.url.startsWith('//') || req.url.startsWith('/\\') || req.url.startsWith('\\')) {
+        req.url = '/' + req.url.replace(/^[/\\]+/, '');
+    }
+    next();
+});
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/nailuj-portfolio/browser'));
 
